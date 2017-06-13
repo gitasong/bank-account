8014c2636bab35002cbbc5e7ddfb4489a39d8cd1
Nicole Freed and Rena Rallis <nicole@>
2017-06-13 16:57:42 -0700

    add helper functions to get current balance from initial deposit and get transaction amount

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 // Business Logic
+initialDeposit = 0;
+depositAmount = 0;
+withdrawalAmount = 0;
+transactionAmount = 0;
+
 function Account(accountName, currentBalance) {
   this.accountName = accountName;
   this.currentBalance = currentBalance;
@@ -8,13 +13,61 @@ Account.prototype.addTransaction = function(transactionAmount) {
   return this.currentBalance + transactionAmount;
 };
 
+function getCurrentBalanceFromInitialDeposit(initialDeposit) {
+    console.log("Initial Deposit = " + initialDeposit);
+  Account.currentBalance = initialDeposit;
+    console.log("Current Balance = " + Account.currentBalance);
+  return Account.currentBalance;
+};
+
+function getTransactionAmount(depositAmount, withdrawalAmount) {
+  transactionAmount = depositAmount - withdrawalAmount;
+  console.log("depositAmount: " + depositAmount);
+  console.log("withdrawalAmount: " + withdrawalAmount);
+  console.log("transaction amount: " + transactionAmount);
+  return transactionAmount;
+};
+
+// function updateBalance(transactionAmount) {
+//   accountCurrentBalance = Account.currentBalance + transactionAmount;
+//     console.log(Account.currentBalance);
+//   return accountCurrentBalance;
+// };
+
 // User Interface
 $(document).ready(function() {
+  var accountName;
+  var initialDeposit;
+  var newAccount;
+  var accountCurrentBalance;
+  var depositAmount;
+  var withdrawalAmount;
+  var transactionTotal;
+
   $("form#registration").submit(function(event) {
     event.preventDefault();
-    var customerName = $("input#customer-name").val();
-      console.log(customerName);
-    var initialDeposit = parseInt($("input#initial-deposit").val());
-      console.log(initialDeposit);
+    accountName = $("input#account-name").val();
+      // console.log(accountName);
+    initialDeposit = parseInt($("input#initial-deposit").val());
+      // console.log(initialDeposit);
+
+    newAccount = new Account(accountName, initialDeposit);
+      console.log("New Account = " + newAccount);
+
+    accountCurrentBalance = getCurrentBalanceFromInitialDeposit(initialDeposit);
+      console.log("Account Current Balance = " + accountCurrentBalance);
   });
+
+  $("form#transaction").submit(function(event) {
+    event.preventDefault();
+    depositAmount = parseInt($("input#deposit-amount").val());
+      // console.log(depositAmount);
+    withdrawalAmount = parseInt($("input#withdrawal-amount").val());
+      // console.log(withdrawalAmount);
+
+    transactionTotal = getTransactionAmount(depositAmount, withdrawalAmount);
+      console.log("Current Transaction Amount = " + transactionTotal);
+  });
+
+  $(".current-balance").text();
 });
